4b4e1f9a0e80cdc551a7d6df4a290c7eb2e64fb8
iamamirshabir <iamamirshabir@gmail.com>
2021-03-09 00:49:53 +0500

    first commit

---
+++ src/main/java/com/pioneer/dips/DipsV1Application.java
@@ -0,0 +1,13 @@
+package com.pioneer.dips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DipsV1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DipsV1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/pioneer/dips/DipsV1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pioneer.dips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DipsV1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
